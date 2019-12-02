63ab0310f21a3455dd6b786c8e8a8574e275aef7
TheMadcowe <56848447+TheMadcowe@users.noreply.github.com>
2019-12-02 12:47:12 +0000

    Add files via upload
    Initial commit

---
+++ FunkifierMk2/ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartWorking();
+        }
+
+        static void StartWorking()
+        {
+            Console.WriteLine("Hello and welcome!\n\nPlease enter the text you'd like to manipulate.\n");
+
+            var text = new Text();
+            var operation = "";
+
+
+            text.InputText = Console.ReadLine();
+
+            Console.WriteLine("\nWhat would you like to do with it?\n\nMAKE UPPERCASE? (U)\nmake lower case? (L)\npErHaPs MaKe It AlL cRaZy? (C)\n" +
+                "Or maybe... just maybe... you want to encode it with a Password? (P)");
+
+            operation = Console.ReadLine().ToUpper();
+
+            if( operation == "P")
+            {
+                Console.Write("Please enter your Password: ");
+
+                text.Password = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Here you go!\n{ text.Manipulator(operation) }\n\nWould you like to go again? (Y/N)");
+
+            string again = Console.ReadLine().ToUpper();
+
+            if(again == "Y")
+            {
+                StartWorking();
+            }
+
+        }
+
+
+        
+    }
+}
---
+++ FunkifierMk2/ConsoleUI/Text.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Text
+    {
+
+        public string InputText { get; set; }
+        public string Password { get; set; }
+
+
+        public string Manipulator(string operation)
+        {
+            string output = "";
+            string inputText = InputText.ToUpper();
+
+            switch(operation)
+            {
+                case ("U"):
+                    output = inputText;
+                    break;
+                case ("L"):
+                    output = inputText.ToLower();
+                    break;
+                case ("C"):
+                    output = Funkify(inputText);
+                    break;
+                case ("P"):
+                    output = Codify(InputText);
+                    break;
+                default:
+                    output = "You had 1 job and you still blew it, congrats.";
+                    break;
+
+               
+            }
+            return output;
+
+        }
+
+        public string Funkify(string text)
+        {
+            string output = "";
+            for(int i = 0; i < text.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    output += text.Substring(i, 1).ToLower();
+                }
+                else
+                {
+                    output += text.Substring(i, 1).ToUpper();
+                }
+            }
+            return output;
+        }
+
+        public string Codify (string text)
+        {
+            string output = "";
+            var passText = text.ToCharArray();
+            var pass = Password.ToCharArray();
+            int passValue = 0;
+
+            for( int i = 0; i < pass.Length; i++)
+            {
+                passValue += (int)pass[i];
+            }
+
+            
+            for(int i = 0; i < text.Length; i++)
+            {
+
+                output += (char)(passText[i] + passValue);
+                Console.WriteLine($"Char: { passText[i] + passValue }");
+            }
+
+            return output;
+        }
+  
+    }
+}
+
